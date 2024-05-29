b42a9281b2aa9769b049944cc94ac08cdc2c1dc7
Tal Weiss <Tweiss747@gmail.com>
2024-05-30 00:52:20 +0300

    Moved About.tsx to sub folder

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,7 +3,7 @@ import { createRef } from "react";
 import Projects from "../components/Projects";
 import Experience from "../components/Experience";
 import Blog from "../components/Blog";
-import About from "../components/About";
+import About from "../components/About/About";
 import App from "../App";
 
 export const routes = [
