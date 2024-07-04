58cb1ea3bc996798022679c7de7d4c6f8997959d
Tal Weiss <Tweiss747@gmail.com>
2024-07-04 00:20:14 -0400

    redesigned experience section to timeline

--- src/components/Experience/Components/WorkCard.tsx
+++ src/components/Experience/Components/WorkCard.tsx
@@ -1,27 +1,37 @@
-import '../../../scss/WorkCard.scss';
-// This will house the work card
+import "../../../scss/WorkCard.scss";
+import "../../../scss/misc.scss";
 interface IProps {
-  name: string;
-  position: string;
-  date: string;
-  company_img: string;
+    name: string;
+    position: string;
+    date: string;
+    company_img: string;
+    cardMargin?: string;
 }
 
-export default function WorkCard({ name, position, date, company_img }: IProps) {
-  return (
-    <div className="work-card">
-      <div className="work-card-content">
-        <div className="work-card-img">
-          <img src={company_img} alt={name} />
+export default function WorkCard({
+    name,
+    position,
+    date,
+    company_img,
+    cardMargin = "right",
+}: IProps) {
+    return (
+        <div className="work-card-container">
+            <div className="work-card-dot"></div>
+            <div className={`work-card-date date-${cardMargin}`}>
+                <h2>{date}</h2>
+            </div>
+            <div className={`work-card position-${cardMargin}`}>
+                <div className="work-card-content">
+                    <div className="work-card-img">
+                        <img src={company_img} alt={name} />
+                    </div>
+                    <div className="work-card-text">
+                        <h1>{name}</h1>
+                        <h2>{position}</h2>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="work-card-text">
-          <h1>{name}</h1>
-          <h2>{position}</h2>
-        </div>
-      </div>
-      <div className="work-card-date">
-        <h2>{date}</h2>
-      </div>
-    </div>
-  );
+    );
 }
--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,21 +1,36 @@
 import WorkCard from "./Components/WorkCard";
 import WorkCardGroup from "./Components/WorkCardGroup";
 export default function Experience() {
-  return (
-    <>
-      <div>
-        <h1>Experience</h1>
-      </div>
-      <div>
-        <WorkCardGroup>
-          <WorkCard
-            name="Google"
-            position="Software Engineer"
-            date="2022 - Present"
-            company_img="https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png"
-          />
-        </WorkCardGroup>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <h1>Experience</h1>
+            </div>
+            <div>
+                <WorkCardGroup>
+                    <WorkCard
+                        name="Google"
+                        position="Software Engineer"
+                        date="2022 - 2024"
+                        company_img="https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png"
+                        cardMargin="right"
+                    />
+                    <WorkCard
+                        name="Google"
+                        position="Software Engineer"
+                        date="2020 - 2021"
+                        company_img="https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png"
+                        cardMargin="left"
+                    />
+                    <WorkCard
+                        name="Google"
+                        position="Software Engineer"
+                        date="2018"
+                        company_img="https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png"
+                        cardMargin="right"
+                    />
+                </WorkCardGroup>
+            </div>
+        </>
+    );
 }
