c2fe2b754667acaccca3a4982752d4af9f08024a
Tal Weiss <Tweiss747@gmail.com>
2024-07-04 22:53:47 -0400

    Implemented modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,26 +1,31 @@
-
+import "../../scss/Modal.scss";
 interface IProps {
-    width: number;
-    height: number;
-    children: JSX.Element | JSX.Element[];
+  children: JSX.Element | JSX.Element[];
+  header?: string;
+  footer?: JSX.Element | JSX.Element[];
+  onClose: () => void;
 }
 
-export default function Modal({ width, height, children }: IProps) {
-    return (
-        <div
-            className="modal"
-            style={{
-                width: `${width}px`,
-                height: `${height}px`,
-            }}
-        >
-            <div>Close</div>
-            {/* Modal content */}
-            <div className="modal-conten">
-                {children}
-            </div>
+export default function Modal({ onClose, header, children }: IProps) {
+  return (
+    <div className="modal-container">
+      <div className="modal">
+        <button onClick={onClose} className="close-btn">
+          <span>✖</span>
+        </button>
+        {/* Modal content */}
+        <div className="modal-content">
+          {header && (
+            <>
+              <div className="modal-header">
+                <h1>{header}</h1>
+              </div>
+              <hr />
+            </>
+          )}
+          {children}
         </div>
-    )
-
-
+      </div>
+    </div>
+  );
 }
