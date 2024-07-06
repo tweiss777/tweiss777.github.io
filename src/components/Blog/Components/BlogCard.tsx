ab91768fd6616d8678e3131803581ac877721fec
Tal Weiss <Tweiss747@gmail.com>
2024-07-05 23:42:10 -0400

    created blog card component

--- src/components/Blog/Components/BlogCard.tsx
+++ src/components/Blog/Components/BlogCard.tsx
@@ -1,15 +1,42 @@
-interface IProps{
-    title: string,    
-    img?: string
+import "../../../scss/BlogCard.scss";
+import { useState } from "react";
+interface IProps {
+    title: string;
+    img?: string;
     link: string;
+    description: string;
 }
+export default function BlogCard({ title, description, img, link }: IProps) {
+    const [hover, setHover] = useState<boolean>(false);
 
+    return (
+        <div
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            className="blog-card-container"
+        >
+            {hover && (
 
-export default function BlogCard() {
-  return (
-    <div className='blog-card-container'>
-      {/* Style the card */}
-      Blog Card
-    </div>
-  );
+                <a href={link}>
+                    <div className="hover-message">
+                        <h2>Click here to read</h2>
+                    </div>
+
+                </a>
+            )}
+            <div className="blog-card">
+                <div className="blog-card-title">
+                    <h1>{title}</h1>
+                </div>
+                <div className="blog-card-body">
+                    <div className="blog-card-img">
+                        <img src={img} alt="blog img" />
+                    </div>
+                    <div className="blog-card-text">
+                        <p>{description}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/components/Blog/Components/BlogCardGroup.tsx
+++ src/components/Blog/Components/BlogCardGroup.tsx
@@ -1,13 +1,8 @@
-interface IProps{
-    children: JSX.Element | JSX.Element[]
+import '../../../scss/BlogCardGroups.scss';
+interface IProps {
+  children: JSX.Element | JSX.Element[];
 }
 
-export default function BlogCardGroup({children}: IProps){
-    return (
-        <div className='blog-card-group'>
-           {children} 
-        </div>
-
-    )
-
+export default function BlogCardGroup({ children }: IProps) {
+  return <div className="blog-card-group">{children}</div>;
 }
