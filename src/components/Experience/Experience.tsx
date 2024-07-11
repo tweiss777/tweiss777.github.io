2b217dc88b1b5fdc492a8b96205d2ea3381584b6
Tal Weiss <Tweiss747@gmail.com>
2024-07-10 21:48:41 -0400

    Mapped data to component

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,35 +1,25 @@
 import WorkCard from "./Components/WorkCard";
 import WorkCardGroup from "./Components/WorkCardGroup";
 import ExperienceHeader from "./Components/ExperienceHeader";
+import workExperience from "../../data/workExperience";
 export default function Experience() {
-    return (
-        <>
-            <ExperienceHeader /> 
-            <div>
-                <WorkCardGroup>
-                    <WorkCard
-                        name="Google"
-                        position="Software Engineer"
-                        date="2022 - 2024"
-                        company_img="https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png"
-                        cardMargin="right"
-                    />
-                    <WorkCard
-                        name="Google"
-                        position="Software Engineer"
-                        date="2020 - 2021"
-                        company_img="https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png"
-                        cardMargin="left"
-                    />
-                    <WorkCard
-                        name="Google"
-                        position="Software Engineer"
-                        date="2018"
-                        company_img="https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png"
-                        cardMargin="right"
-                    />
-                </WorkCardGroup>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <ExperienceHeader />
+      <div>
+        <WorkCardGroup>
+          {workExperience.map((work, i) => (
+            <WorkCard
+              key={i}
+              name={work.name}
+              position={work.position}
+              date={work.date}
+              company_img={work.company_img}
+              cardMargin={work.cardMargin}
+            />
+          ))}
+        </WorkCardGroup>
+      </div>
+    </>
+  );
 }
