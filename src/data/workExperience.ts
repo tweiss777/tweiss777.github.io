0096b34f25ca52831ee0e2a63bcec6f6f20465a3
Tal Weiss <Tweiss747@gmail.com>
2024-07-10 21:13:19 -0400

    created structure for each work experience.

---
+++ src/data/workExperience.ts
@@ -0,0 +1,36 @@
+type WorkExperience = {
+  name: string;
+  position: string;
+  date: string;
+  company_img: string;
+  cardMargin: string;
+  description?: string[];
+};
+
+const workCards: WorkExperience[] = [
+  {
+    name: "Google",
+    position: "Software Engineer",
+    date: "2022 - 2024",
+    company_img:
+      "https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png",
+    cardMargin: "right",
+  },
+  {
+    name: "Google",
+    position: "Software Engineer",
+    date: "2020 - 2021",
+    company_img:
+      "https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png",
+    cardMargin: "left",
+  },
+  {
+    name: "Google",
+    position: "Software Engineer",
+    date: "2018",
+    company_img:
+      "https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png",
+    cardMargin: "right",
+  },
+];
+export default workCards;
