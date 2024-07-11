7f4bf9b98f39f3aa9d2ffbd41d51e643251b1804
Tal Weiss <Tweiss747@gmail.com>
2024-07-10 22:09:43 -0400

    call modal

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -3,19 +3,28 @@ import WorkCardGroup from "./Components/WorkCardGroup";
 import ExperienceHeader from "./Components/ExperienceHeader";
 import workExperience from "../../data/workExperience";
 import { useState } from "react";
+import Modal from "../Modal/Modal";
 export default function Experience() {
-    const [currentWork, setCurrentWork] = useState(null);
-    function handleOnClick(work: any) {
-
+  const [currentWork, setCurrentWork] = useState<any>(null);
+  function handleOnClick(work: any) {
+      setCurrentWork(work);
     }
+
   return (
     <>
       <ExperienceHeader />
+      {currentWork && (
+        <Modal
+          header={currentWork?.name}
+          onClose={() => setCurrentWork(null)}
+        >
+        </Modal>
+      )}
       <div>
         <WorkCardGroup>
           {workExperience.map((work, i) => (
             <WorkCard
-              handleOnClick={handleOnClick} 
+              handleOnClick={handleOnClick}
               key={i}
               name={work.name}
               position={work.position}
