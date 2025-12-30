6707c151649d79f2e9f242388e57b16e91b719fd
Tal Weiss <Tweiss747@gmail.com>
2025-12-30 14:03:01 +0200

    Reconfigured base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/',
   server: {
     host: '0.0.0.0',
     port: 5000,
