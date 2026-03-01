fd7b69e2df5c0e398d24d6b00cbac1f8ad0548b6
Tal Weiss <Tweiss747@gmail.com>
2026-02-28 21:35:48 -0500

    Injected slide fade transitions for about and workcard components.

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,30 +6,66 @@ import ProgressBarGroup from "../ProgressBarGroup/ProgressBarGroup";
 import ProgressBar from "../ProgressBar/ProgressBar";
 import techStack from "../../data/techStack";
 import skills from "../../data/skills";
+import { CSSTransition } from "react-transition-group";
+import '../../scss/TransitionStyles.scss'
+import  useScrollTrigger from '../../hooks/useScrollTrigger'
+
 export default function About() {
+  const { ref: infoRef, isVisible: infoVisible } = useScrollTrigger();
+  const { ref: techRef, isVisible: techVisible } = useScrollTrigger();
+  const { ref: skillsRef, isVisible: skillsVisible } = useScrollTrigger();
+
   return (
     <>
-      <Info
-        name="Tal Weiss"
-        title="Full Stack Developer"
-        about="Full-stack engineer with 5 years of experience helping startups and growing teams design and build scalable, high-performance web applications. I translate complex business requirements into clean, intuitive user experiences and reliable, cloud-native systems using React, Node.js, and AWS—enabling teams to ship faster and scale with confidence."
-      />
-      <TechStack>
-        {techStack.map(({img, name}, index) => (
-          <TechStackCard
-            key={index}
-            name={name} 
-            img={img}
-          />
-        ))}
-      </TechStack>
-      <ProgressBarGroup>
-        {skills.map(({ name, complete }, index) => (
-        <div className="progress-bar-col">
-          <ProgressBar key={index} complete={complete} name={name} />
-        </div>))}
-          
-      </ProgressBarGroup>
+      <CSSTransition
+          timeout={400}
+          classNames='slide'
+          nodeRef={infoRef}
+          in={infoVisible}
+      >
+        <div className={'content'} ref={infoRef}>
+        <Info
+            name="Tal Weiss"
+            title="Full Stack Developer"
+            about="I'm a full-stack engineer with 5 years of experience helping startups and growing teams design and build scalable, high-performance web applications. I translate complex business requirements into clean, intuitive user experiences and reliable, cloud-native systems using React, Node.js, and AWS—enabling teams to ship faster and scale with confidence."
+        />
+
+        </div>
+      </CSSTransition>
+      <CSSTransition
+          timeout={400}
+          classNames='slide'
+          nodeRef={techRef}
+          in={techVisible}
+      >
+        <div ref={techRef} className={'content'}>
+          <TechStack>
+            {techStack.map(({img, name}, index) => (
+                <TechStackCard
+                    key={index}
+                    name={name}
+                    img={img}
+                />
+            ))}
+          </TechStack>
+        </div>
+      </CSSTransition>
+      <CSSTransition
+          timeout={400}
+          classNames='slide'
+          nodeRef={skillsRef}
+          in={skillsVisible}
+      >
+        <div ref={skillsRef} className={'content'}>
+          <ProgressBarGroup>
+            {skills.map(({ name, complete }, index) => (
+                <div className="progress-bar-col">
+                  <ProgressBar key={index} complete={complete} name={name} />
+                </div>))}
+
+          </ProgressBarGroup>
+        </div>
+      </CSSTransition>
     </>
   );
 }
--- src/components/Experience/Components/WorkCard.tsx
+++ src/components/Experience/Components/WorkCard.tsx
@@ -1,6 +1,7 @@
 import "../../../scss/WorkCard.scss";
 import { JSX } from "react";
-
+import useScrollTrigger from "../../../hooks/useScrollTrigger.ts";
+import {CSSTransition} from "react-transition-group";
 interface IProps {
   name: string;
   position: string;
@@ -23,23 +24,33 @@ export default function WorkCard({
   function onClickExperience() { const work = { name, position, date, company_img, description: description ?? null, };
     handleOnClick(work);
   }
+  const { ref, isVisible} = useScrollTrigger()
   return (
-    <div onClick={onClickExperience} className="work-card-container">
-      <div className="work-card-dot"></div>
-      <div className={`work-card-date date-${cardMargin}`}>
-        <h2>{date}</h2>
-      </div>
-      <div className={`work-card position-${cardMargin}`}>
-        <div className="work-card-content">
-          <div className="work-card-img">
-            <img src={company_img} alt={name} />
-          </div>
-          <div className="work-card-text">
-            <h1>{name}</h1>
-            <h2>{position}</h2>
-          </div>
-        </div>
-      </div>
-    </div>
+      <CSSTransition
+          timeout={400}
+          in={isVisible}
+          nodeRef={ref}
+          classNames="slide"
+          >
+       <div ref={ref} className='content'>
+         <div onClick={onClickExperience} className="work-card-container">
+           <div className="work-card-dot"></div>
+           <div className={`work-card-date date-${cardMargin}`}>
+             <h2>{date}</h2>
+           </div>
+           <div className={`work-card position-${cardMargin}`}>
+             <div className="work-card-content">
+               <div className="work-card-img">
+                 <img src={company_img} alt={name} />
+               </div>
+               <div className="work-card-text">
+                 <h1>{name}</h1>
+                 <h2>{position}</h2>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+      </CSSTransition>
   );
 }
