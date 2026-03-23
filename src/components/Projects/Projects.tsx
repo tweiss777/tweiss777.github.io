44cce333a37fc4aad343438c84395ee27d36be57
Tal Weiss <Tweiss747@gmail.com>
2026-03-23 19:35:46 -0400

    typo fix updates to code

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -7,51 +7,12 @@ export default function Projects() {
             <ProjectsHeader />
             <div>
                 <ProjectCardGroup>
-                    <ProjectCard title={'Salesforce MCP Server'} description={
+                    {projects.map((project) =>{ return <ProjectCard title={project.title} description={
                         <p>
-                            Developed a salesforce mcp server in python that connects to your Salesforce
-                            organization and performs CRUD operations for your lead, opportunity, contact, and account records. Custom fields are also supported
+                            {project.description}
                         </p>
-                    } link={'https://github.com/tweiss777/salesforce-mcp'}
-                    />
-                    <ProjectCard
-                        title="SmartJobs (Python Program W/ Dotnet Web App)"
-                        description={
-                                <p>
-                                    Developed a .Net Core MVC based web application using bootstrap
-                                    that analyzes job descriptions against a resume, determining
-                                    which job is a good fit, and recommends what skills are missing or
-                                    needs improvement based on matching and missing keywords.
-                                </p>
-                        }
-                        link="https://github.com/tweiss777/CapstoneAPI/tree/master"
-                    />
-                    <ProjectCard
-                        title="TypeOff (iOS Game)"
-                        description={
-                            <p> A type-racing game that measures a user’s typing speed and accuracy.</p>
-                        }
-                        link="https://bitbucket.org/tweiss777/typeoff/src/master/"
-                    />
-                    <ProjectCard
-                        title="Admin Dashboarrd"
-                        description={
-                            <p>
-                            A basic admin dashboard with user authentication and authorization, CRUD operations, and filtering using React for the frontend and Fastify for the backend
-                            </p>
-                        }
-                        link="https://github.com/tweiss777/ReactAdminDashboard"
-                    />
-
-                    <ProjectCard
-                        title="Web calculator"
-                        description={
-                            <p>
-                            A calculator web app mimicking a real calculator based on HTML, CSS, and JavaScript and the iOS theme. 
-                            </p>
-                        }
-                        link="https://github.com/tweiss777/basic-calculator"
-                    />
+                    } link={project.link}
+                    />})}
                 </ProjectCardGroup>
             </div>
         </>
---
+++ src/data/projecsts.ts
@@ -0,0 +1,33 @@
+interface Project {
+    title: string;
+    description: string;
+    link: string;
+}
+
+const projects: Project[] = [
+    {
+        title: 'Salesforce MCP Server',
+        description: 'Developed a salesforce mcp server in python that connects to your Salesforce organization and performs CRUD operations for your lead, opportunity, contact, and account records. Custom fields are also supported',
+        link: 'https://github.com/tweiss777/salesforce-mcp'
+    },
+    {
+        title: 'SmartJobs (Python Program W/ Dotnet Web App)',
+        description: 'Developed a .Net Core MVC based web application using bootstrap that analyzes job descriptions against a resume, determining which job is a good fit, and recommends what skills are missing or needs improvement based on matching and missing keywords.',
+        link: 'https://github.com/tweiss777/CapstoneAPI/tree/master'
+    },
+    {
+        title: 'TypeOff (iOS Game)',
+        description: 'A type-racing game that measures a user\'s typing speed and accuracy.',
+        link: 'https://bitbucket.org/tweiss777/typeoff/src/master/'
+    },
+    {
+        title: 'Admin Dashboard',
+        description: 'A basic admin dashboard with user authentication and authorization, CRUD operations, and filtering using React for the frontend and Fastify for the backend',
+        link: 'https://github.com/tweiss777/ReactAdminDashboard'
+    },
+    {
+        title: 'Web calculator',
+        description: 'A calculator web app mimicking a real calculator based on HTML, CSS, and JavaScript and the iOS theme.',
+        link: 'https://github.com/tweiss777/basic-calculator'
+    }
+];
